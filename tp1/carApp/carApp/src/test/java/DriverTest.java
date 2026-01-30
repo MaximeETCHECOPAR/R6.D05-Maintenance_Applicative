a161d5807c8ea3873f59667bde81e0a8a7099577
Maxime Etchecopar <metchecop001@iutbayonne.univ-pau.fr>
2026-01-30 12:28:15 +0100

    Correction des tests de DriverTest

--- tp1/carApp/carApp/src/test/java/DriverTest.java
+++ tp1/carApp/carApp/src/test/java/DriverTest.java
@@ -5,10 +5,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class DriverTest {
 
     private Driver driver;
+    private Car car;
 
     @BeforeEach
     public void setUp() {
         driver = new Driver("John", 20);
+        car = new Car("Sedan", "Bleu");
     }
 
     @Test
@@ -18,23 +20,20 @@ public class DriverTest {
 
     @Test
     public void testStartCar() {
-        Car voiture = new Car("Sedan", "Bleu");
         driver.startCar();
         // How to assert that the demarrerVoiture method was called?
     }
 
     @Test
     public void testStopCar() {
-        Car voiture = new Car("Sedan", "Bleu");
         driver.stopCar();
         // How to assert that the arreterVoiture method was called?
     }
 
     @Test
     public void testSwitchSpeed() {
-        Car voiture = new Car("Sedan", "Bleu");
 
-        driver.switchSpeed(voiture, 80);
-        assertThat(voiture.getSpeed()).isEqualTo(80);
+        driver.switchSpeed(car, 80);
+        assertThat(car.getSpeed()).isEqualTo(80);
     }
 }
