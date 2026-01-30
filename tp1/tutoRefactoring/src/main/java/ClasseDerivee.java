0775e4836c503625f19cc8575cef83d426e124b4
Maxime Etchecopar <metchecop001@iutbayonne.univ-pau.fr>
2026-01-30 09:35:29 +0100

    changement

--- tp1/tutoRefactoring/src/main/java/ClasseDerivee.java
+++ tp1/tutoRefactoring/src/main/java/ClasseDerivee.java
@@ -1,14 +1,7 @@
 public class ClasseDerivee extends BaseClass{
+    private int member;
     public static void main(String[] args) {
         ClasseDerivee subject = new ClasseDerivee();
         System.out.println("Doublement 21. Résultat = " + subject.doubleValue(21));
-    }
-
-    /**
-     * @param number  - le nombre à doubler
-     * @return  - 2*number
-     */
-    private int doubleValue(int number) {
-        return number + number;
     }
 }
